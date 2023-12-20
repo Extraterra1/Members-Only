b7226a65e2b14285db6ee0a416393904efdc4fff
Carlos Pires <extraterra1@gmail.com>
2023-12-20 11:29:32 +0000

    dev: user model

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const validateEmail = (email) => {
+  const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, minLength: 5, unique: true, validate: [validateEmail, 'Please enter a valid email address'] },
+  role: { type: String, enum: ['guest', 'member', 'admin'], default: 'guest' }
+});
+
+module.exports = mongoose.model('User', userSchema);
