059d38999ff6a110480f3973afc44b9a59d58a01
Carlos Pires <extraterra1@gmail.com>
2023-12-20 14:25:48 +0000

    feat: submit messages

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcryptjs");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 const User = require("../models/userModel");
+const Post = require("../models/postModel");
 const passport = require("passport");
 
 /* GET home page. */
@@ -15,17 +16,37 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/newMessage", isLoggedIn, (req, res) => {
-  res.render("newMessage", { title: "New Message" });
+  if (req.user.role === "guest") return res.redirect("/upgrade?notMember");
+  res.render("newMessage", { title: "New Message", user: req.user });
 });
 
-router.post("/new", (req, res) => {
-  messages.unshift({
-    text: req.body.msg,
-    user: req.body.username,
-    added: moment().format("ddd, MMM Do YYYY (hh:mm)"),
-  });
-  res.redirect("/");
-});
+router.post(
+  "/newMessage",
+  body("title", "Title must be at least 3 characters long")
+    .trim()
+    .isLength({ min: 2 }),
+  body("msg", "Message must be at least 3 characters long")
+    .trim()
+    .escape()
+    .isLength({ min: 3 }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const newPost = new Post({
+      title: req.body.title,
+      msg: req.body.msg,
+      author: req.user.id,
+    });
+    if (!errors.isEmpty())
+      return res.render("newMessage", {
+        title: "New Message",
+        user: req.user,
+        post: newPost,
+        err: errors.array(),
+      });
+    await newPost.save();
+    res.redirect("/");
+  }),
+);
 
 router.get("/login", (req, res) => {
   if (req.isAuthenticated()) return res.redirect("/");
