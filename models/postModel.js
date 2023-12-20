7d99d105c857e7c0277de2961f25c1514cd15825
Carlos Pires <extraterra1@gmail.com>
2023-12-20 13:52:12 +0000

    dev: add date to post model

--- models/postModel.js
+++ models/postModel.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
   title: { type: String, required: true, minLength: 3, maxLength: 15 },
   msg: { type: String, required: true, minLength: 3 },
-  author: { type: mongoose.Types.ObjectId, ref: 'User', required: true }
+  added: { type: Date, default: Date.now },
+  author: { type: mongoose.Types.ObjectId, ref: "User", required: true },
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,7 @@ router.get("/login", (req, res) => {
 router.post(
   "/login",
   passport.authenticate("local", {
-    successRedirect: "/?good",
+    successRedirect: "/",
     failureMessage: true,
     failureRedirect: "/login",
   }),
